f3be6ee7b47c18f074dd5c9ac8346ff896fde878
FafaThomas <tdeclarino@gmail.com>
2023-10-17 11:15:24 +0800

    main

--- Email.php
+++ Email.php
@@ -1,22 +1,41 @@
 <?php
-if(isset($_POST['submit'])) {
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $type = $_POST['type'];
     $message = $_POST['message'];
 
-    $to = 'support@icontroltech.com';
-    $subject = 'Contact Form Submission - ' . $name . ' - ' . $type;
-    $body = "Name: " . $name . "\n" .
-            "Email: " . $email . "\n" .
-            "Type: " . $type . "\n" .
-            "Message: " . $message;
+    // SMTP server settings
+    $smtp_host = 'mail.icontroltech.com';
+    $smtp_port = 465;
+    $smtp_username = 'inquiry@icontroltech.com'; // Your SMTP username (your email address)
+    $smtp_password = 'icti_inquiry2023'; // Your SMTP password
+    
+    // Recipient email address
+    $recipient_email = 'support@icontroltech.com'; // Change to the recipient's email address
 
-    $headers = "From: " . $email . "\r\n" .
-               "Reply-To: " . $email . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
+    // Create an email header
+    $headers = "From: $email\r\nReply-To: $email";
+    
+    // Set the subject as a combination of "name" and "type"
+    $subject = "Contact Form - $name ($type)";
 
-    mail($to, $subject, $body, $headers);
-    header("Location: Email.php?mailsend");
+    // Compose the email message
+    $email_message = $message;
+
+    // Configure SMTP settings
+    ini_set('SMTP', $smtp_host);
+    ini_set('smtp_port', $smtp_port);
+    ini_set('sendmail_from', $smtp_username);
+
+    // Attempt to send the email
+    if (mail($recipient_email, $subject, $email_message, $headers)) {
+        echo 'Message has been sent';
+    } else {
+        echo 'Message could not be sent';
+    }
+} else {
+    echo 'Invalid request';
 }
 ?>
